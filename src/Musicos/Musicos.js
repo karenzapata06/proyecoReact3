031c516efe75c1963bc47ab6b5cf2d639edba47d
407 <407@academicos.cesde.edu.co>
2022-10-12 17:41:26 -0500

    cambios

--- src/Musicos/Musicos.js
+++ src/Musicos/Musicos.js
@@ -4,20 +4,15 @@ export function Musicos (){
     //Un arreglo de objetos es formato comun al consumir un api 
     let musicos=[
         {
-            nombre:"Tilo Wolff",
-            rol:"Composición, Programación, teclados, piano, trompeta, guitarra",
-            url:"https://firebasestorage.googleapis.com/v0/b/spotify-9b3a0.appspot.com/o/Tilo%20Wolff.jpg?alt=media&token=e2a6178b-c2bc-4bec-a886-68a994e7b451",
+            nombre:"Jesse Huerta",
+            rol:"Composición, piano,guitarra",
+            url:"https://firebasestorage.googleapis.com/v0/b/spotify2-29277.appspot.com/o/Jesse_Huerta_(cropped).jpg?alt=media&token=f5513c40-39f8-4e1b-923d-91f91864e7dd",
         },
         {
-            nombre:"Anne Nurmi",
-            rol:"Teclados",
-            url:"https://firebasestorage.googleapis.com/v0/b/spotify-9b3a0.appspot.com/o/Anne%20Nurmi2.jpg?alt=media&token=d7953ccf-1686-40a7-8ce1-1e5e9450df61",
-        },
-        {
-            nombre:"Tilo y Anne",
-            rol:"Vocalistas",
-            url:"https://firebasestorage.googleapis.com/v0/b/spotify-9b3a0.appspot.com/o/lacrimosaVocalistas.png?alt=media&token=7b5dcc22-bb08-4d4c-ae2f-3e4228ffc421",
-        }
+            nombre:"Joy Huerta",
+            rol:"Vocalista Principal",
+            url:"https://firebasestorage.googleapis.com/v0/b/spotify2-29277.appspot.com/o/joy.jpg?alt=media&token=3a133664-1cb4-4d62-9bc9-84bb03cf4bb2",
+        }  
     ]
 
     let conciertoMemorables=[
--- src/SubMenu/SubMenu.js
+++ src/SubMenu/SubMenu.js
@@ -4,20 +4,20 @@ export function SubMenu(){
             <div className="container">
                 <div className="row text-center">
                     <div className="col-12 col-md-4 border p-3">
-                        <a href="https://www.discogs.com/es/artist/93828-Lacrimosa">
+                        <a href="https://www.letras.com/jesse-joy/discografia/">
                         <i className="bi bi-cassette-fill fs-1 my-2 text-dark"></i>
                         </a>
                     
                     </div>
                         
                     <div className="col-12 col-md-4 border p-3">
-                        <a href="http://lacrimosa.com/biographie/">
+                        <a href="https://www.last.fm/es/music/Jesse+&+Joy/+wiki">
                         <i className="bi bi-hourglass-split fs-1 my-2 text-dark"></i>
                         </a>
                     
                     </div>
                     <div className="col-12 col-md-4 border p-3">
-                        <a href="https://es.wikipedia.org/wiki/Lacrimosa_(banda)">
+                        <a href="https://musica.itematika.com/biografia/i316/jesse--joy.html">
                         <i className="bi bi-person-fill fs-1 my-2 text-dark"></i>
                         </a>
                     
